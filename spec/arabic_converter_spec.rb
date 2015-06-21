53cf38f2150fe6ec10893b65efec779c757959bb
Ashwini Meshram <Ashu@Ashwinis-MacBook-Pro.local>
2015-06-22 08:15:27 +0900

    spec file: number below 100

--- spec/arabic_converter_spec.rb
+++ spec/arabic_converter_spec.rb
@@ -5,6 +5,18 @@ describe TestProject do
 	describe "Convert Arabic numerals to English numerals" do
 		subject {testproject}
 
+		context "Arabic numeral to English numeral(ex.1): Below 1000" do
+			let(:testproject) { TestProject.new(:num => 537) }
+
+			specify { expect(subject.arabic_to_eng).to eq("five hundred thirty seven")}
+		end
+
+		context "Arabic numeral to English numeral(ex.2): Below 1000" do
+			let(:testproject) { TestProject.new(:num => 909) }
+
+			specify { expect(subject.arabic_to_eng).to eq("nine hundred nine")}
+		end
+
 		context "Arabic numeral to English numeral: Below 100" do
 			let(:testproject) { TestProject.new(:num => 72) }
 
