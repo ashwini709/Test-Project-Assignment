b75df3f72d075105da33a9310b1d997b40a2b15a
Ashwini Meshram <Ashu@Ashwinis-MacBook-Pro.local>
2015-06-22 08:52:18 +0900

    removing redundant test examples; updating README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Test-Project-Assignment
 * Convert Arabic numerals to English numerals
 * This is a sample code for positive integers
+* Command for running ruby code from command line: 
+```
+ruby arabic_converter_cli.rb 467
+```
+* Command to run rspec: 
+```
+rspec spec/arabic_converter_spec.rb
+```
--- spec/arabic_converter_spec.rb
+++ spec/arabic_converter_spec.rb
@@ -5,28 +5,16 @@ describe TestProject do
 	describe "Convert Arabic numerals to English numerals" do
 		subject {testproject}
 
-		context "Arabic numeral to English numeral(ex.1): Above 1000" do
+		context "Arabic numeral to English numeral: Above 1000" do
 			let(:testproject) { TestProject.new(:num => 8956354792561) }
 
 			specify { expect(subject.arabic_to_eng).to eq("eight trillion nine hundred fifty six billion three hundred fifty four million seven hundred ninety two thousand five hundred sixty one")}
-		end
-
-		context "Arabic numeral to English numeral(ex.2): Above 1000" do
-			let(:testproject) { TestProject.new(:num => 1537) }
-
-			specify { expect(subject.arabic_to_eng).to eq("one thousand five hundred thirty seven")}
 		end
 
 		context "Arabic numeral to English numeral: Below 1000" do
 			let(:testproject) { TestProject.new(:num => 909) }
 
 			specify { expect(subject.arabic_to_eng).to eq("nine hundred nine")}
-		end
-
-		context "Arabic numeral to English numeral: Below 100" do
-			let(:testproject) { TestProject.new(:num => 72) }
-
-			specify { expect(subject.arabic_to_eng).to eq("seventy two")}
 		end
 
 		context "Arabic numeral to English numeral: Below 20" do
